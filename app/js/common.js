eff9be1e1bbc1b12a9711f41d27a9b1ea9ead669
Max Spassky <maxspas1@gmail.com>
2017-04-25 19:06:44 +0300

    25.04.2017 Tuesday (2)

--- app/js/common.js
+++ app/js/common.js
@@ -31,8 +31,24 @@ var sticky = function(){
 }();
 
 
-$('.choisen-box, .feedbk-box').owlCarousel({
+$('.choisen-box').owlCarousel({
 	autoPlay: true
+})
+
+$('.feedbk-box').owlCarousel({
+	items: 4,
+	itemsDesktopSmall: [1500, 3],
+	itemsTablet: [980, 2],
+	itemsMobile: [650, 1],
+	margin: 20,
+	lazyLoad: true,
+	slideSpeed: 2500,
+	rewindSpeed: 3000,
+	paginationSpeed: 2000,
+	autoPlay: 5000,
+	stopOnHover: true,
+	pagination: true,
+	navigation: false
 })
 
 $('#gallery-list').unitegallery();
@@ -128,4 +144,35 @@ $('#gallery-list').unitegallery();
 
 })
 
-
+/*
+ymaps.ready(init); // карта соберется после загрузки скрипта и элементов
+var myMap; // заглобалим переменную карты чтобы можно было ею вертеть из любого места
+function init () { // функция - собиралка карты и фигни
+   myMap = new ymaps.Map("map", { // создаем и присваиваем глобальной переменной карту и суем её в див с id="map"
+        center: [55.76, 37.64], // ну тут центр
+        behaviors: ['default', 'scrollZoom'], // скроллинг колесом
+        zoom: 10 // тут масштаб
+   });
+   myMap.controls // добавим всяких кнопок, в скобках их позиции в блоке
+        .add('zoomControl', { left: 5, top: 5 }) //Масштаб
+        .add('typeSelector') //Список типов карты
+        .add('mapTools', { left: 35, top: 5 }) // Стандартный набор кнопок
+        .add('searchControl'); // Строка с поиском
+    }
+    
+myPlacemark0 = new ymaps.Placemark([55.752577,37.632134], { // Создаем метку с такими координатами и суем в переменную
+        balloonContent: 'Заголовок метки 1Немного инфы о том, о сем. Лорем ипсум чото там.' // сдесь содержимое балуна в формате html, все стили в css
+	}, {
+	iconImageHref: 'img/icon1.png', // картинка иконки
+	iconImageSize: [64, 64], // размер иконки
+	iconImageOffset: [-32, -64], // позиция иконки
+	balloonContentSize: [270, 99], // размер нашего кастомного балуна в пикселях
+	balloonLayout: "default#imageWithContent", // указываем что содержимое балуна кастомная херь
+	balloonImageHref: 'img/ballon1.png', // Картинка заднего фона балуна
+	balloonImageOffset: [-65, -89], // смещание балуна, надо подогнать под стрелочку
+	balloonImageSize: [260, 89], // размер картинки-бэкграунда балуна
+	balloonShadow: false
+});
+ Добавляем метки на карту 
+myMap.geoObjects
+	.add(myPlacemark0);*/
